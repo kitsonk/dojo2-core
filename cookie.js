f4e9e49b30d5b1ec5e8077694df6e9aae60b6fb1
Colin Snover <github.com@zetafleet.com>
2012-12-10 06:23:21 +0000

    Reimplemented dojo/cookie module.

--- cookie.js
+++ cookie.js
@@ -1,98 +1,143 @@
-define(["./_base/kernel", "./regexp"], function(dojo, regexp){
+define([], function () {
+	// TODO: Put these utility functions somewhere else. dojo/string perhaps?
 
-// module:
-//		dojo/cookie
-
-/*=====
-var __cookieProps = {
-	// expires: Date|String|Number?
-	//		If a number, the number of days from today at which the cookie
-	//		will expire. If a date, the date past which the cookie will expire.
-	//		If expires is in the past, the cookie will be deleted.
-	//		If expires is omitted or is 0, the cookie will expire when the browser closes.
-	// path: String?
-	//		The path to use for the cookie.
-	// domain: String?
-	//		The domain to use for the cookie.
-	// secure: Boolean?
-	//		Whether to only send the cookie on secure connections
-};
-=====*/
-
-
-dojo.cookie = function(/*String*/name, /*String?*/ value, /*__cookieProps?*/ props){
-	// summary:
-	//		Get or set a cookie.
-	// description:
-	//		If one argument is passed, returns the value of the cookie
-	//		For two or more arguments, acts as a setter.
-	// name:
-	//		Name of the cookie
-	// value:
-	//		Value for the cookie
-	// props:
-	//		Properties for the cookie
-	// example:
-	//		set a cookie with the JSON-serialized contents of an object which
-	//		will expire 5 days from now:
-	//	|	require(["dojo/cookie", "dojo/json"], function(cookie, json){
-	//	|		cookie("configObj", json.stringify(config, {expires: 5 }));
-	//	|	});
-	//
-	// example:
-	//		de-serialize a cookie back into a JavaScript object:
-	//	|	require(["dojo/cookie", "dojo/json"], function(cookie, json){
-	//	|		config = json.parse(cookie("configObj"));
-	//	|	});
-	//
-	// example:
-	//		delete a cookie:
-	//	|	require(["dojo/cookie"], function(cookie){
-	//	|		cookie("configObj", null, {expires: -1});
-	//	|	});
-	var c = document.cookie, ret;
-	if(arguments.length == 1){
-		var matches = c.match(new RegExp("(?:^|; )" + regexp.escapeString(name) + "=([^;]*)"));
-		ret = matches ? decodeURIComponent(matches[1]) : undefined; 
-	}else{
-		props = props || {};
-// FIXME: expires=0 seems to disappear right away, not on close? (FF3)  Change docs?
-		var exp = props.expires;
-		if(typeof exp == "number"){
-			var d = new Date();
-			d.setTime(d.getTime() + exp*24*60*60*1000);
-			exp = props.expires = d;
-		}
-		if(exp && exp.toUTCString){ props.expires = exp.toUTCString(); }
-
-		value = encodeURIComponent(value);
-		var updatedCookie = name + "=" + value, propName;
-		for(propName in props){
-			updatedCookie += "; " + propName;
-			var propValue = props[propName];
-			if(propValue !== true){ updatedCookie += "=" + propValue; }
-		}
-		document.cookie = updatedCookie;
+	/**
+	 * Escapes a string to be used within a regular expression.
+	 * @returns {string} Escaped string.
+	 */
+	function escapeString(/**string*/ string) {
+		return string.replace(/[-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&');
 	}
-	return ret; // String|undefined
-};
 
-dojo.cookie.isSupported = function(){
-	// summary:
-	//		Use to determine if the current browser supports cookies or not.
-	//
-	//		Returns true if user allows cookies.
-	//		Returns false if user doesn't allow cookies.
+	/**
+	 * Counts the number of instances of `needle` found inside `haystack`.
+	 *
+	 * @param haystack
+	 * String to search.
+	 *
+	 * @param needle
+	 * String to look for.
+	 *
+	 * @returns {number} Number of hits.
+	 */
+	function count(/**string*/ haystack, /**string*/ needle) {
+		var hits = 0,
+			lastIndex = haystack.indexOf(needle);
 
-	if(!("cookieEnabled" in navigator)){
-		this("__djCookieTest__", "CookiesAllowed");
-		navigator.cookieEnabled = this("__djCookieTest__") == "CookiesAllowed";
-		if(navigator.cookieEnabled){
-			this("__djCookieTest__", "", {expires: -1});
+		while (lastIndex > -1) {
+			++hits;
+			lastIndex = haystack.indexOf(needle, lastIndex + 1);
 		}
-	}
-	return navigator.cookieEnabled;
-};
 
-return dojo.cookie;
+		return hits;
+	}
+
+	/**
+	 * Options that may be used to remove a cookie.
+	 *
+	 * @name removeCookieOptions
+	 *
+	 * @property {string} path
+	 * The path to use for the cookie.
+	 */
+
+	/**
+	 * Options that may be used to set a cookie.
+	 *
+	 * @name cookieOptions
+	 *
+	 * @property {Date} expires
+	 * The date at which the cookie should expire. By default, the cookie will expire when the browser closes.
+	 *
+	 * @property {number} maxAge
+	 * The number of seconds from now that the cookie should expire. By default, the cookie will expire when the
+	 * browser closes.
+	 *
+	 * @property {string} path
+	 * The path to use for the cookie.
+	 *
+	 * @property {string} domain
+	 * The domain to use for the cookie.
+	 *
+	 * @property {boolean} secure
+	 * Whether or not to only send the cookie over secure connections.
+	 */
+
+	/**
+	 * Creates a well-formed cookie options string.
+	 * @returns {string}
+	 */
+	function createCookieOptions(/**cookieOptions*/ options) {
+		var optionsString = '';
+		for (var k in options) {
+			if (k === 'maxAge') {
+				k = 'max-age';
+			}
+
+			optionsString += '; ' + encodeURIComponent(k);
+
+			if (k === 'secure') {
+				// do nothing, secure is a boolean flag
+			}
+			else if (k === 'expires') {
+				optionsString += '=' + encodeURIComponent(options[k].toUTCString());
+			}
+			else {
+				optionsString += '=' + encodeURIComponent(options[k]);
+			}
+		}
+
+		return optionsString;
+	}
+
+	if (!navigator.cookieEnabled) {
+		return null;
+	}
+
+	var cookie = document.cookie,
+		longAgo = new Date(1970, 0, 1).toUTCString();
+
+	/**
+	 * An interface for getting and setting cookies based on the DOM Storage API.
+	 */
+	return {
+		/**
+		 * The number of cookies that are currently set.
+		 */
+		get length() {
+			return count(cookie, '; ');
+		},
+
+		/**
+		 * Gets the key of the cookie at the given index.
+		 * @returns {?string}
+		 */
+		key: function (/**number*/ index) {
+			var keyValuePair = cookie.split('; ')[index];
+			return keyValuePair ? /^([^=]+)/.exec(keyValuePair)[0] : null;
+		},
+
+		/**
+		 * Gets the value of a cookie.
+		 * @returns {?string}
+		 */
+		getItem: function (/**string*/ key) {
+			var match = new RegExp('(?:^|; )' + escapeString(key) + '=([^;]*)').exec(cookie);
+			return match ? decodeURIComponent(match[1]) : null;
+		},
+
+		/**
+		 * Sets the value of a cookie.
+		 */
+		setItem: function (/**string*/ key, /**string*/ data, /**cookieOptions=*/ options) {
+			document.cookie = encodeURIComponent(key) + '=' + encodeURIComponent(data) + createCookieOptions(options);
+		},
+
+		/**
+		 * Removes a cookie.
+		 */
+		removeItem: function (/**string*/ key, /**removeCookieOptions=*/ options) {
+			document.cookie = encodeURIComponent(key) + '=' + '; expires=' + longAgo + createCookieOptions(options);
+		}
+	};
 });
