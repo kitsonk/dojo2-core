022d555015c4b06cd45c263b018b2d06d56050d4
Colin Snover <github.com@zetafleet.com>
2012-12-24 03:02:22 +0000

    Enable loading Node.js modules with maybe-AMD dependencies.

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
-define(["dojo/has"], function(has){
-	if(!has("host-node")){
-		throw new Error("node plugin failed to load because environment is not Node.js");
+define([ 'dojo/has' ], function (has) {
+	if (!has('host-node')) {
+		throw new Error('node plugin failed to load because environment is not Node.js');
 	}
 
 	return {
@@ -12,16 +12,41 @@ define(["dojo/has"], function(has){
 		//	|		var fileData = fs.readFileSync("foo.txt", "utf-8");
 		//	|	});
 
-		load: function(/*string*/ id, /*Function*/ require, /*Function*/ load){
+		load: function (/*string*/ id, /*Function*/ require, /*Function*/ callback) {
 			// summary:
 			//		Standard AMD plugin interface. See https://github.com/amdjs/amdjs-api/wiki/Loader-Plugins
 			//		for information.
 
-			if(!require.nodeRequire){
-				throw new Error("Cannot find native require function");
+			if (!require.nodeRequire) {
+				throw new Error('Cannot find native require function');
 			}
 
-			load(require.nodeRequire(id));
+			var oldDefine = define,
+				result;
+
+			// Some modules may attempt to detect an AMD environment and register themselves with the loader;
+			// this breaks when loading a non-AMD module that loads one of these multi-environment modules, so
+			// hide the global define during the loading process
+			define = undefined;
+
+			try {
+				result = require.nodeRequire(id);
+			}
+			finally {
+				// nodeRequire might throw, it might not; in either case we need to make sure define is fixed up
+				// immediately after the module is loaded
+				define = oldDefine;
+			}
+
+			callback(result);
+		},
+
+		normalize: function (/**string*/ id) {
+			if (id.charAt(0) === '.') {
+				id = require.inspect('baseUrl') + id;
+			}
+
+			return id;
 		}
 	};
 });
