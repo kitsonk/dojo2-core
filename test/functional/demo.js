016b25ccae4b863058462aae28cc21783cd9780f
Colin Snover <github.com@zetafleet.com>
2013-02-04 18:18:46 +0000

    Push up a (bogus) functional test example so people see how it works.

---
+++ test/functional/demo.js
@@ -0,0 +1,28 @@
+define([
+	'teststack!tdd',
+	'teststack/lib/assert',
+	'require'
+], function (test, assert, require) {
+	test.suite('demo', function () {
+		test.before(function () {
+			// TODO: Fix this invalid URL generation
+			return this.remote.get('http://localhost:9000/' + require.toAbsMid('./demo.html'));
+		});
+
+		test.test('it works', function () {
+			var remote = this.remote;
+
+			return remote.elementById('testInput')
+				.clickElement()
+				.keys('hello')
+				.end()
+				.elementById('submitButton')
+				.clickElement()
+				.end()
+				.alertText().then(function (text) {
+					assert.isEqual(text, 'hello', 'alert shows text from input');
+				})
+				.dismissAlert();
+		});
+	});
+});
